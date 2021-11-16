d2ae873f692eddbbe2ab05e40e6b3f8458ba6a44
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-11-16 11:18:22 +0100

    yea clean it all up...new init point

--- _dev/_build.js
+++
@@ -1,6 +0,0 @@
-const v_execute = require('v_execute');
-
-// Simple listing
-v_execute("bash -c \" node config/_configure.js mode=dev ; start bash -c \\\"nodemon --exec npm run build \\\";\" exit ;");
-// Simple listing
-v_execute(" bash -c \"npm run __server \" exit ");
